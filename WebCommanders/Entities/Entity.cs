2b45c31ef7cb933a35de9d2521b602770e880e60
Timothy Moore <mtimothy983@gmail.com>
2017-05-22 10:33:37 -0700

    Split Math to SharpMath2, add gitignore

--- WebCommanders/Entities/Entity.cs
+++ WebCommanders/Entities/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SharpMath2;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace WebCommanders.Entities
 {
     public class Entity
     {
+        public Polygon2 Bounds;
     }
 }
